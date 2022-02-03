d4f4834ed9036bc94762f8fb4711a2f0475dd8ce
Danilo Amorim <danbamorim@gmail.com>
2022-02-03 18:16:52 -0300

    mudança view

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -16,36 +16,51 @@
             <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
 
                 <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
-                    <h2><span class="fs-4">Blog</span></h2>
+                    <h2><span class="fs-4">D.B.A</span></h2>
 
                 </a>
             </div>
 
             <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-                <h1 class="display-4 fw-normal">Título</h>
+                <h1 class="display-4 fw-normal"style="font-family: FreeMono, monospace">Poemas</h>
             </div>
         </header>
-           
-        <p>hdfshuhhgfjhsdjfghjdhsgj</p>
+        </br>
+
+        <h4 style="font-family: FreeMono, monospace">3/02/2022</h4> 
+        </br>
+        </br>
+        <p style="font-family: FreeMono, monospace"> Ola Mundo </p>
+
 
         <style>
             h1 {
                 color: #800000;
                 text-align: center;
             }
+
             h2 {
-                color: darkolivegreen;
+                color: black;
             }
 
-            p{ 
-                color: black; 
+            h4 {
+                color: black;
                 text-align: center;
+                font-size: 1.4em;
+            }
+
+            p { 
+                text-align: center;
+                font-size: 1.8em;
                 
             }
-            body{ 
-                background-color: #dcdcdc;
+
+            body {
+                background-color: #f4f9fc;
             }
         </style>
+
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
