ea4e8ef1fabc771c8b192ebcdf53bb1c6cf55547
Danilo Amorim <danbamorim@gmail.com>
2022-02-06 18:36:52 -0300

    Domingo 06/02

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -7,33 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-</head>
-
-<body>
-    <div class="container py-3">
-        <header>
-            <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
-
-                <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
-                    <h2><span class="fs-4">D.B.A</span></h2>
-
-                </a>
-            </div>
-
-            <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-                <h1 class="display-4 fw-normal"style="font-family: FreeMono, monospace">Poemas</h>
-            </div>
-        </header>
-        </br>
-
-        <h4 style="font-family: FreeMono, monospace">3/02/2022</h4> 
-        </br>
-        </br>
-        <p style="font-family: FreeMono, monospace"> Ola Mundo </p>
-
-
-        <style>
+    <style>
             h1 {
                 color: #800000;
                 text-align: center;
@@ -52,15 +26,35 @@
             p { 
                 text-align: center;
                 font-size: 1.8em;
-                
             }
 
             body {
                 background-color: #f4f9fc;
             }
         </style>
+</head>
 
+<body>
+    <div class="container py-3">
+        <header>
+            <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
 
+                <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
+                    <h2><span class="fs-4">blog</span></h2>
+
+                </a>
+            </div>
+
+            <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+                <h1 class="display-4 fw-normal"style="font-family: FreeMono, monospace">Poemas</h>
+            </div>
+        </header>
+        </br>
+
+        <h4 style="font-family: FreeMono, monospace">3/02/2022</h4> 
+        </br>
+        </br>
+        <p style="font-family: FreeMono, monospace"> Ola Mundo </p>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () { 
-    return view(view:'site.home');
+    return view('site.home');
 })->name('site.home');
 
