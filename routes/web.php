e8a5cd614471d1a2bb76f9f043e93f0b2bf6e9c4
Danilo Amorim <danbamorim@gmail.com>
2022-03-08 20:31:17 -0300

    dia 8

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/','App\Http\Controllers\BookController@index'); 
 
 Route::resource('postagens','App\Http\Controllers\PostagensController');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
