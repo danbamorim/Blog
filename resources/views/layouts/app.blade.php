f48e88a5a23f5e9c828ba789f8cd74b848bc08ec
Danilo Amorim <danbamorim@gmail.com>
2022-04-19 20:48:53 -0300

    dia 19 de Abril

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -125,26 +125,25 @@
                         </li>
                         @endif
                         @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Teste</a>
+                            <a class="nav-link" href="{{ route('posts.create') }}">Criar</a>
                         </li>
-
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('posts.index') }}">Listar</a>
+                        </li>
+                        
+                        <li class="nav-item">
                             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                             document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
-
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                 @csrf
                             </form>
-                        </div>
                         </li>
+
+
+
                         @endguest
                     </ul>
                 </div>
